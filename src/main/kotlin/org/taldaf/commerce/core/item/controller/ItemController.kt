fd432712325ab1f0c1aebac8b4c9bb3ff2a0af6c
dltjdgh97 <dltjdgh97@daou.co.kr>
2025-03-23 12:38:13 +0900

    refactor: item controller 를 kotlin 파일로 변환

---
+++ src/main/kotlin/org/taldaf/commerce/core/item/controller/ItemController.kt
@@ -0,0 +1,21 @@
+package org.taldaf.commerce.core.item.controller
+
+import lombok.RequiredArgsConstructor
+import lombok.extern.slf4j.Slf4j
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import org.taldaf.commerce.core.item.model.ItemDto
+import org.taldaf.commerce.core.item.service.ItemQueryService
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+class ItemController(
+    private val itemQueryService: ItemQueryService
+) {
+    @GetMapping("/item/{itemId}")
+    fun getItem(@PathVariable itemId: Long): ItemDto {
+        return itemQueryService.getItem(itemId)
+    }
+}
