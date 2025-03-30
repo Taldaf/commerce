8902f71b74801783278b0653bf25866f9686456a
SunghoLee98 <dltjdgh69@gmail.com>
2025-03-30 21:17:58 +0900

    chore: test 시 test 프로필 사용

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,13 +34,21 @@ dependencies {
 	// SpringDoc OpenAPI (Swagger) dependencies
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
 	implementation("org.springdoc:springdoc-openapi-starter-common:2.4.0")
-	
+
 	runtimeOnly("org.postgresql:postgresql")
+
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	testRuntimeOnly("com.h2database:h2")
+
+	testCompileOnly("org.projectlombok:lombok")
+	testAnnotationProcessor("org.projectlombok:lombok")
+
 }
 
 kotlin {
